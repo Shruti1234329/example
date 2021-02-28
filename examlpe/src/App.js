d7b48da30fc783b3c4984352d8b8bc479e606df9
Shrut1234329 <shrutivishghosh@gmail.com>
2021-02-28 15:34:06 +0530

    example

---
+++ examlpe/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <img src= "/image/read.jpg" alt="Page not found"/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ examlpe/src/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Study Center</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Product</a>
+      </li>
+      <li class="nav-dark"> 
+        <a class="nav-link " href="#">About Us</a>
+      </li>
+      <li class="nav-dark">
+        <a class="nav-link " href="#">Support</a>
+      </li>
+     
+      
+    </ul>
+    
+  </div>
+</nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
